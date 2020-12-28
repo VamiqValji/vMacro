2f95dcf1e52169785656234e3405b0e5dca5862a
Vamiq <vamiq.valji@gmail.com>
2020-12-28 17:16:16 -0500

    Repeat / Broken
    - The app can now repeat what you did back to you (controls keyboard)
    - The app isn't working on some apps as those apps have detection against keyloggers..

--- keys.py
+++ keys.py
@@ -1,5 +1,7 @@
 import pynput
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, Controller
+
+keyboard = Controller()
 
 keysPressedList = []
 
@@ -19,17 +21,23 @@ def writeFile():
 
 
 def on_press(key):
-    global keysPressedList
-    keysPressedList.append(key)
+
     print(f"{key} pressed.")
 
 
 def on_release(key):
+    global keysPressedList
     if key == Key.esc:
         print("\nExited vMacro.\n")
         writeFile()
         return False  # break out of loop
-    print(f"{key} released.")
+    elif key == Key.enter:
+        for k in keysPressedList:
+            keyboard.press(k)
+            keyboard.release(k)
+    else:
+        keysPressedList.append(key)
+        print(f"{key} released.")
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
