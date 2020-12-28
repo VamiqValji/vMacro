ab47c10d3b310df37fb0696c4ed6defa46d52375
Vamiq <vamiq.valji@gmail.com>
2020-12-28 16:58:10 -0500

    Adds keypresses to txt file
    records and then adds keypresses to a txt file

--- keys.py
+++ keys.py
@@ -1,13 +1,33 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+keysPressedList = []
+
+print("\n\n\nHit escape to exit!\n\nStarting vMacro.\n")
+
+
+def writeFile():
+    f = open("keysPressed.txt", "w")
+    keysPressedStr = ""
+    print(keysPressedList)
+    for keyPressed in keysPressedList:
+        keysPressedStr += f"'{keyPressed}'"
+    keysPressedStr = ((keysPressedStr.replace(
+        "'''", ",")).strip("''")).replace("'", "")
+    f.write(keysPressedStr)
+    f.close()
+
 
 def on_press(key):
+    global keysPressedList
+    keysPressedList.append(key)
     print(f"{key} pressed.")
 
 
 def on_release(key):
     if key == Key.esc:
+        print("\nExited vMacro.\n")
+        writeFile()
         return False  # break out of loop
     print(f"{key} released.")
 
