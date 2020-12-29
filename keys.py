5b45414953f218628cb3df476f558793d808c984
Vamiq <vamiq.valji@gmail.com>
2020-12-28 22:07:46 -0500

    Backtrack
    time.sleep is having trouble with the replay function, gonna back track a bit

--- keys.py
+++ keys.py
@@ -1,4 +1,5 @@
 import time
+import os
 import pynput
 from pynput.keyboard import Key, Listener, Controller
 
@@ -12,6 +13,7 @@ print(startTime)
 print("\n\n\nHit escape to exit!\n\nStarting vMacro.\n")
 
 pressed = False
+replaying = False
 
 
 def writeFile():
@@ -19,6 +21,8 @@ def writeFile():
     keysPressedStr = ""
     print(keysPressedList)
     for keyPressed in keysPressedList:
+        if "Key" in (f"{keyPressed}"):
+            keyPressed = f",{keyPressed},"
         keysPressedStr += f"'{keyPressed}'"
     keysPressedStr = ((keysPressedStr.replace(
         "'''", ",")).strip("''")).replace("'", "")
@@ -42,34 +46,61 @@ def stopCounter():
 
 def on_press(key):
     global pressed
-    pressed = True
-    if pressed == False:
-        print(f"{key} pressed.")
-        startCounter()
-    else:
-        print(f"{key} held down.")
+    global replaying
+    if replaying == False:
+        pressed = True
+        if pressed == False:
+            print(f"{key} pressed.")
+            startCounter()
+        else:
+            print(f"{key} held down.")
+    if key == Key.enter:
+        pressed = False
+        if replaying == False:
+            replay()
+            return False
 
 
 def replay():
     global keysPressedList
+    global replaying
+    replaying = True
+    # writeFile()
+    # os.startfile("keysPressed.txt")
+    makeEven = 1
     for k in keysPressedList:
-        keyboard.press(k)
-        time.sleep(0.3)
-        keyboard.release(k)
+        try:
+            print(f"\n\n-REPLAY-\n")
+            if k % 2 != 0:
+                makeEven = k - 1
+            k = keysPressedList.index(k) * 2
+            keyboard.press(keysPressedList[makeEven])
+            print(f"{keysPressedList[makeEven]} being pressed.")
+            time.sleep(1)
+            print(f"Waiting {keysPressedList[k + 1]} seconds.")
+            keyboard.release(keysPressedList[makeEven])
+            print(f"{keysPressedList[makeEven]} released.")
+            if (keysPressedList.index(k) / 2 == len(keysPressedList)):
+                keyboard.release(keysPressedList[makeEven])
+                print("\nMacro Complete.\n")
+                return False
+        except:
+            pass
 
 
 def on_release(key):
     global keysPressedList
+    global replaying
     if key == Key.esc:
         print("\nExited vMacro.\n")
         writeFile()
+        os.startfile("keysPressed.txt")
         return False  # break out of loop
-    elif key == Key.enter:
-        replay()
     else:
-        keysPressedList.append(key)
-        keysPressedList.append(f"{round(stopCounter(), 2)}")
-        print(f"{key} released.")
+        if replaying == False:
+            keysPressedList.append(key)
+            keysPressedList.append(f"{round(stopCounter(), 2)}")
+            print(f"{key} released.")
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
